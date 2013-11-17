c81db688285232b148c17cc0ce5c1a05d295ceae
esneider <dariosn@gmail.com>
2013-11-17 17:59:48 -0300

    there and back again

--- code.js
+++ code.js
@@ -4,20 +4,30 @@ $(document).ready(function() {
     var peer = new Peer({key: 'lwjd5qra8257b9'});
 
     if (typeof peer_id === 'undefined') {
+
         peer.on('open', function(id) {
 
             $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?id=' + id);
             $('#the_link').text('Click here');
         });
-    }
 
-    peer.on('connection', function(conn) {
+        peer.on('connection', function(conn) {
 
-        console.log('Received a new connection');
-    });
+            console.log('Received a new connection');
 
-    if (typeof peer_id !== 'undefined') {
+            conn.send('Hello world!');
+        });
+
+    } else {
 
         var conn = peer.connect(peer_id);
+
+        conn.on('open', function() {
+
+            conn.on('data', function(data) {
+
+                console.log('Received:', data);
+            });
+        });
     }
 });
