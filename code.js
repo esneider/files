f988cec8c0162ed584aaefc05416b5ca202a4afd
esneider <dariosn@gmail.com>
2013-11-17 18:30:46 -0300

    oh no

--- code.js
+++ code.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
     var peer_id = $.url().param('id');
     var peer = new Peer({key: 'lwjd5qra8257b9'});
 
+    peer.on('error', function(err) {
+
+        console.log('Damnation! ' + err.type);
+    });
+
     peer.on('open', function(id) {
 
         $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?id=' + id);
