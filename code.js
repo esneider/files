f95940dec76cb79f8ad2edb61af1dec11d1655f3
esneider <dariosn@gmail.com>
2013-11-17 17:23:51 -0300

    Server-Client

--- code.js
+++ code.js
@@ -1,10 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     var peer = new Peer({key: 'lwjd5qra8257b9'});
 
     peer.on('open', function(id) {
+
         console.log('My peer ID is: ' + id);
+
+        $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?id=' + id);
+        $('#the_link').text('Click here');
     });
 
+    peer.on('connection', function(conn) {
 
+        console.log('Received a new connection');
+    });
+
+    console.log('Got id=' + $.url().param('id'));
 });
