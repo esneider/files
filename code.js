543165af1c56274e64ae73533e199ab642ebee06
esneider <dariosn@gmail.com>
2013-11-17 17:54:46 -0300

    test

--- code.js
+++ code.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     var peer_id = $.url().param('id');
     var peer = new Peer({key: 'lwjd5qra8257b9'});
 
-    peer.on('open', function(id) {
+    if (typeof peer_id === 'undefined') {
+        peer.on('open', function(id) {
 
-        $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?id=' + id);
-        $('#the_link').text('Click here');
-    });
+            $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?id=' + id);
+            $('#the_link').text('Click here');
+        });
+    }
 
     peer.on('connection', function(conn) {
 
