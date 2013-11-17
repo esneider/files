91ec9b4796b265d1e7fa71e7a09181d7655bb69f
esneider <dariosn@gmail.com>
2013-11-17 18:55:52 -0300

    c'mon bilbo

--- code.js
+++ code.js
@@ -1,52 +1,67 @@
-$(document).ready(function() {
+function server_peer() {
 
-    var server_id = $.url().param('id');
-    var client_id = undefined;
     var peer = new Peer({key: 'lwjd5qra8257b9'});
 
     peer.on('error', function(err) {
 
-        console.log('Damnation! ' + err.type);
+        console.log('(client_peer) Damnation! ' + err.type);
     });
 
     peer.on('open', function(id) {
 
-        client_id = id;
-
-        $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?id=' + id);
+        $('#the_link').prop('href', 'http://esneider.github.io/files/index.html?sid=' + id);
         $('#the_link').text('Click here');
     });
 
-    if (typeof server_id === 'undefined') {
+    peer.on('connection', function(conn) {
 
-        peer.on('connection', function(conn) {
+        var client_id = conn.peer;
 
-            console.log('Received a new connection');
+        conn.destroy();
 
-            console.log(conn);
+        var conn2 = peer.connect(client_id);
 
-            conn.on('data', function(data) {
+        conn2.on('open', function() {
 
-                console.log('Received:', data);
-            });
-
-            conn.send('Hello world form server');
+            conn2.send('Hey there! :)');
         });
+    });
+}
+
+function client_peer(server_id) {
+
+    var peer = new Peer({key: 'lwjd5qra8257b9'});
+    var conn = peer.connect(server_id);
+
+    conn.on('open', function() {
+
+        conn.destroy();
+    });
+
+    peer.on('error', function(err) {
+
+        console.log('(client_peer) Damnation! ' + err.type);
+    });
+
+    peer.on('connection', function(conn) {
+
+        conn.on('data', function(data) {
+
+            console.log('Receiced:', data);
+        });
+    });
+}
+
+$(document).ready(function() {
+
+    var server_id = $.url().param('sid');
+
+    if (typeof server_id === undefined) {
+
+        server_peer();
 
     } else {
 
-        var conn = peer.connect(server_id);
-
-        conn.on('open', function() {
-
-            console.log('Opened connection');
-
-            conn.on('data', function(data) {
-
-                console.log('Received:', data);
-            });
-
-            conn.send('Hello world from client');
-        });
+        client_peer(server_id);
     }
 });
